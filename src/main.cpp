939b83029a1be403b9830376d26c74c6cd021c3d
felixUOB <zx23695@bristol.ac.uk>
2025-05-22 23:11:48 +0100

    first vertex shader added

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,20 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+
+float vertices[] = {
+   -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f,  0.5f, 0.0f
+};  
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -15,6 +29,13 @@ void processInput(GLFWwindow *window)
 void render() {
    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
    glClear(GL_COLOR_BUFFER_BIT);
+
+   unsigned int VBO;
+   glGenBuffers(1, &VBO);  // Generate a buffer object
+
+   glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind the buffer object to the GL_ARRAY_BUFFER target
+
+   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Copy vertex data to the binded buffer
 }
 
 int main() {
@@ -41,6 +62,30 @@ int main() {
    }  
 
    glViewport(0, 0, 800, 600);
+
+   // SHADERS
+
+   unsigned int vertexShader;
+   vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+   glCompileShader(vertexShader);
+
+   int  success;
+   char infoLog[512];
+   glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+   if(!success) {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+      glfwTerminate();
+      return -1;
+   } else {
+      std::cout << "Shader compiled successfully!" << std::endl;
+   }
+
+   // GAME LOOP
 
    while(!glfwWindowShouldClose(window)) {   
       processInput(window);
