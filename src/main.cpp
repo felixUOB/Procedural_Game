5a14a41701cb5be73adad4c51c34db6a260e914a
felixUOB <zx23695@bristol.ac.uk>
2025-05-23 00:37:36 +0100

    more comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 
 const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+   "layout (location = 0) in vec3 aPos;\n"
+   "void main()\n"
+   "{\n"
+   "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+   "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
+   "out vec4 FragColor;\n"
+   "void main()\n"
+   "{\n"
+   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+   "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -106,26 +106,23 @@ int main() {
    glGenBuffers(1, &VBO);
    glGenBuffers(1, &EBO);
    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-   glBindVertexArray(VAO);
+   glBindVertexArray(VAO); // VAOs store all subsequent VBO calls so we dont need to keep binding the VBOs
 
    glBindBuffer(GL_ARRAY_BUFFER, VBO);
    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // EBOs allow us to reuse vertex data
    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-   glEnableVertexAttribArray(0);
+   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // tell OpenGL how to interperate vertex data
+   glEnableVertexAttribArray(0); // enable the vertex attribute 0 (position) in the shader
 
-   // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-   glBindBuffer(GL_ARRAY_BUFFER, 0); 
-
-   // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-   //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
+   glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind the VBO so we can bind other VBOs without affecting this one
    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
    glBindVertexArray(0); 
+   // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
    // game loop
    // ---------
@@ -136,7 +133,7 @@ int main() {
       glClear(GL_COLOR_BUFFER_BIT);
 
       glUseProgram(shaderProgram);
-      glBindVertexArray(VAO);
+      glBindVertexArray(VAO); // bind the VAO (contains VBO and EBO already) to draw the triangles
       glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
       
       glfwSwapBuffers(window);
