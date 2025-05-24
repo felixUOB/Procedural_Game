8cd7cd914c73186fa2eb524a0e45baf1812cfb23
felixUOB <zx23695@bristol.ac.uk>
2025-05-24 23:29:10 +0100

    layering textures and adding interaction with up and down keys

--- src/shaders/f_shader.glsl
+++ src/shaders/f_shader.glsl
@@ -1,11 +1,14 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 outColor;
+in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float dist;
 
 void main() {
-    FragColor = texture(ourTexture, TexCoord);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(-TexCoord.x, TexCoord.y)), dist);
+    // FragColor = vec4(ourColor, 1.0);
 }
