fba85f374678e7b4b2506213e59594bae272885e
felixUOB <zx23695@bristol.ac.uk>
2025-05-26 18:32:59 +0100

    added more

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,20 @@ int main() {
       0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
       -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-};
+   };
+
+   glm::vec3 cubePositions[] = {
+      glm::vec3( 0.0f,  0.0f,  0.0f), 
+      glm::vec3( 2.0f,  5.0f, -15.0f), 
+      glm::vec3(-1.5f, -2.2f, -2.5f),  
+      glm::vec3(-3.8f, -2.0f, -12.3f),  
+      glm::vec3( 2.4f, -0.4f, -3.5f),  
+      glm::vec3(-1.7f,  3.0f, -7.5f),  
+      glm::vec3( 1.3f, -2.0f, -2.5f),  
+      glm::vec3( 1.5f,  2.0f, -2.5f), 
+      glm::vec3( 1.5f,  0.2f, -1.5f), 
+      glm::vec3(-1.3f,  1.0f, -1.5f)  
+   };
 
    unsigned int indices[] = {
       0, 1, 3,   // first triangle
@@ -186,17 +199,6 @@ int main() {
    ourShader.setInt("texture1", 0);
    ourShader.setInt("texture2", 1);
 
-   // matrix stuff
-   // ------------
-   std::cout << "================= MATRIX STUFF =======================" << std::endl;
-   glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-   glm::mat4 trans = glm::mat4(1.0f);
-   trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-   trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-   vec = trans * vec;
-   std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;   
-   std::cout << "======================================================" << std::endl;
-
    // render loop
    // -----------
    while (!glfwWindowShouldClose(window))
@@ -224,21 +226,28 @@ int main() {
       glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
       glm::mat4 view          = glm::mat4(1.0f);
       glm::mat4 projection    = glm::mat4(1.0f);
-      model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+      // model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
       view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
       projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
       // retrieve the matrix uniform locations
-      unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-      unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view");
-      // pass them to the shaders (3 different ways)
-      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-      glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-      // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+      ourShader.setMat4("view", view);
       ourShader.setMat4("projection", projection);
 
       // render container
       glBindVertexArray(VAO);
-      glDrawArrays(GL_TRIANGLES, 0, 36);
+      for(unsigned int i = 0; i < 10; i++)
+      {
+         // calculate the model matrix for each object and pass it to shader before drawing
+         glm::mat4 model = glm::mat4(1.0f);
+         model = glm::translate(model, cubePositions[i]);
+         float angle = 20.0f * i; 
+         if(i % 3 == 0)  // every 3rd iteration (including the first) we set the angle using GLFW's time function.
+            angle = glfwGetTime() * 25.0f;
+         model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+         ourShader.setMat4("model", model);
+         
+         glDrawArrays(GL_TRIANGLES, 0, 36);           
+      }
 
       // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
       // -------------------------------------------------------------------------------
