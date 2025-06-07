2b10e003c203ab621a978b536a77f96282d34557
felixUOB <zx23695@bristol.ac.uk>
2025-06-07 16:07:55 +0100

    added seperate texture loader file

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 
 #include "services/shader.h"
 #include "services/camera.h"
+#include "services/texture_loader.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "tools/stb_image.h"
@@ -166,55 +167,8 @@ int main() {
    // load and generate textures
    // --------------------------
 
-   unsigned int texture1, texture2;
-   // texture 1
-   // ---------
-   glGenTextures(1, &texture1);
-   glBindTexture(GL_TEXTURE_2D, texture1); 
-   // set the texture wrapping parameters
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-   // set texture filtering parameters
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-   // load image, create texture and generate mipmaps
-   int width, height, nrChannels;
-   stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-   // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-   unsigned char *data = stbi_load("src/elements/container.jpg", &width, &height, &nrChannels, 0);
-   if (data)
-   {
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-      glGenerateMipmap(GL_TEXTURE_2D);
-   }
-   else
-   {
-      std::cout << "Failed to load texture" << std::endl;
-   }
-   stbi_image_free(data);
-   // texture 2
-   // ---------
-   glGenTextures(1, &texture2);
-   glBindTexture(GL_TEXTURE_2D, texture2);
-   // set the texture wrapping parameters
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-   // set texture filtering parameters
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-   // load image, create texture and generate mipmaps
-   data = stbi_load("src/elements/waltuh.jpg", &width, &height, &nrChannels, 0);
-   if (data)
-   {
-      // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-      glGenerateMipmap(GL_TEXTURE_2D);
-   }
-   else
-   {
-      std::cout << "Failed to load texture" << std::endl;
-   }
-   stbi_image_free(data);
+   unsigned int texture1 = TextureLoader::LoadTexture("src/elements/container.jpg");
+   unsigned int texture2 = TextureLoader::LoadTexture("src/elements/waltuh.jpg");
 
    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
    // -------------------------------------------------------------------------------------------
